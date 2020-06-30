59e4c1b9e75f59e8e45b1235e40673c8044f978a
Riften <943275366@qq.com>
2020-06-30 15:05:17 +0800

    test recursive

--- server/handler/templates.go
+++ server/handler/templates.go
@@ -7,13 +7,14 @@ import (
 
 func (h *HttpHandler) rendIndex(w http.ResponseWriter) {
 	fmt.Println("render index")
-	tmpl, err := h.repo.FetchTemplate([]string{"layout.html"})
+	tmpl, err := h.repo.FetchTemplate([]string{"layout.html", "index.html"})
 	if err != nil {
 		fmt.Println("Error when load template layout.html: ", err)
 		return
 	}
-	err = tmpl.ExecuteTemplate(w, "layout", "Hello world")
+	err = tmpl.ExecuteTemplate(w, "layout", "layout: Hello world")
 	if err != nil {
 		fmt.Println("Error when execute template layout.html: ", err)
 	}
+	err = tmpl.ExecuteTemplate(w, "index", "index: Hello world")
 }
