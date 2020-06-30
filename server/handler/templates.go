f19ef98589c27b72f26758ac5a3bab902188e46f
Riften <943275366@qq.com>
2020-06-30 10:25:34 +0800

    bugfix

--- server/handler/templates.go
+++ server/handler/templates.go
@@ -6,12 +6,13 @@ import (
 )
 
 func (h *HttpHandler) rendIndex(w http.ResponseWriter) {
+	fmt.Println("render index")
 	tmpl, err := h.repo.FetchTemplate([]string{"layout.html"})
 	if err != nil {
 		fmt.Println("Error when load template layout.html: ", err)
 		return
 	}
-	err = tmpl.Execute(w, "模板测试")
+	err = tmpl.ExecuteTemplate(w, "layout", "Hello world")
 	if err != nil {
 		fmt.Println("Error when execute template layout.html: ", err)
 	}
